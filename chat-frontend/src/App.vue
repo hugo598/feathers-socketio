2850b059c645de29e687013b20b9af105724a1dc
Hugo Casildo <hugo598@gmail.com>
2017-05-08 23:15:13 -0500

    Solo el bakcend

---
+++ chat-frontend/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <h1>Chat</h1>
+    <ul>
+      <li v-for="message in messages">
+        {{messages}}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style>
+  body{
+    margin: 50px;
+    font-family: sans-serif;
+  }
+</style>
